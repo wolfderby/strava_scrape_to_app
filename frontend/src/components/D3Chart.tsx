25b669a032ead080ba4d972cdcf59ff067952b27
wolfderby <OllieRadburn@gmail.com>
2023-09-25 10:29:32 -0400

    sorted by date

--- frontend/src/components/D3Chart.tsx
+++ frontend/src/components/D3Chart.tsx
@@ -7,6 +7,7 @@ type DataProps = {
   distance_raw: number;
   date: string;
   start_date: string;
+  start_date_local_raw: number;
   start_time: string;
   moving_time: string;
   moving_time_raw: number;
@@ -17,71 +18,76 @@ interface D3ChartProps {
   data: DataProps[];
 }
 
+
 const D3Chart: React.FC<D3ChartProps> = ({ data }) => {
   const chartRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
+
+    // Sort the data by date
+    data.sort((a, b) => a.start_date_local_raw - b.start_date_local_raw);
+
     // Clear the previous SVG if it exists
     d3.select(chartRef.current).selectAll("*").remove();
 
     if (data && data.length > 0) {
       const svg = d3.select(chartRef.current)
-                    .append('svg')
-                    .attr('width', 500)
-                    .attr('height', 300);
+        .append('svg')
+        .attr('width', 500)
+        .attr('height', 300);
 
       // Create tooltip div
       const tooltip = d3.select(chartRef.current)
-                        .append("div")
-                        .attr("class", "tooltip")
-                        .style("opacity", 0);
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
 
       // Map distances to an array
       const distances: number[] = data.map(d => d.distance_raw);
 
       // Define scales
       const xScale = d3.scaleBand()
-                       .domain(Array.from(Array(distances.length).keys()).map(String))
-                       .range([0, 500])
-                       .padding(0.1);
-      
+        .domain(data.map((d, i) => String(new Date(d.start_time).toLocaleDateString())))
+        .range([0, 500])
+        .padding(0.1);
+
       const yScale = d3.scaleLinear()
-                       .domain([0, d3.max(distances) as number])
-                       .range([300, 0]);
+        .domain([0, d3.max(distances) as number])
+        .range([300, 0]);
 
       // Draw bars
       const bars = svg.selectAll('rect')
         .data(data) // Use data directly instead of distances
         .enter()
         .append('rect')
-        .attr('x', (d, i) => xScale(String(i)) as number)
+        .attr('x', (d) => xScale(new Date(d.start_time).toLocaleDateString()) as number)
         .attr('y', d => yScale(d.distance_raw) as number)
         .attr('width', xScale.bandwidth())
         .attr('height', d => 300 - yScale(d.distance_raw) as number)
         .attr('fill', 'blue');
- 
 
-// Add tooltips
-bars.on("mouseover", function (event, d) {
-  // Calculate pace in MM:SS/mi
-  const pace = (d.moving_time_raw / d.distance_raw * 1609.34); // pace in seconds per mile
-  const paceMinutes = Math.floor(pace / 60);
-  const paceSeconds = Math.round(pace % 60).toString().padStart(2, '0');
 
-  tooltip.style("opacity", 1)
-         .html(`
+      // Add tooltips
+      bars.on("mouseover", function (event, d) {
+        // Calculate pace in MM:SS/mi
+        const pace = (d.moving_time_raw / d.distance_raw * 1609.34); // pace in seconds per mile
+        const paceMinutes = Math.floor(pace / 60);
+        const paceSeconds = Math.round(pace % 60).toString().padStart(2, '0');
+
+        tooltip.style("opacity", 1)
+          .html(`
           <strong>Date:</strong> ${d.start_date} <br>
           <strong>Time:</strong> ${new Date(d.start_time).toLocaleTimeString()} <br>
           <strong>Duration:</strong> ${d.moving_time} <br>
           <strong>Distance:</strong> ${d.distance} miles <br>
           <strong>Pace:</strong> ${paceMinutes}:${paceSeconds} / mi
           `)
-         .style("left", `${event.pageX}px`)
-         .style("top", `${event.pageY}px`);
-})
-.on("mouseout", function () {
-  tooltip.style("opacity", 0);
-});
+          .style("left", `${event.pageX}px`)
+          .style("top", `${event.pageY}px`);
+      })
+        .on("mouseout", function () {
+          tooltip.style("opacity", 0);
+        });
 
 
       // Add click event to bars
@@ -92,18 +98,18 @@ bars.on("mouseover", function (event, d) {
 
       // Add X-axis label
       svg.append('text')
-         .attr('x', 250)
-         .attr('y', 320)
-         .attr('text-anchor', 'middle')
-         .text('Activity Index');
+        .attr('x', 250)
+        .attr('y', 320)
+        .attr('text-anchor', 'middle')
+        .text('Activity Index');
 
       // Add Y-axis label
       svg.append('text')
-         .attr('transform', 'rotate(-90)')
-         .attr('x', -150)
-         .attr('y', -40)
-         .attr('text-anchor', 'middle')
-         .text('Distance');
+        .attr('transform', 'rotate(-90)')
+        .attr('x', -150)
+        .attr('y', -40)
+        .attr('text-anchor', 'middle')
+        .text('Distance');
     }
   }, [data]);
 
