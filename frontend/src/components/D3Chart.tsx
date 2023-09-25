9015577d5be6c1126414e60c2c5e659e472cfc35
wolfderby <OllieRadburn@gmail.com>
2023-09-25 09:31:50 -0400

    Distance added

--- frontend/src/components/D3Chart.tsx
+++ frontend/src/components/D3Chart.tsx
@@ -2,8 +2,17 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
-const D3Chart = ({ data }) => {
-  const chartRef = useRef(null);
+// Define the type for your data, assuming each data object has a distance_raw property that is a number
+type DataProps = {
+  distance_raw: number;
+};
+
+interface D3ChartProps {
+  data: DataProps[];
+}
+
+const D3Chart: React.FC<D3ChartProps> = ({ data }) => {
+  const chartRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     // Clear the previous SVG if it exists
@@ -15,29 +24,52 @@ const D3Chart = ({ data }) => {
                     .attr('width', 500)
                     .attr('height', 300);
 
+      // Create tooltip div
+      const tooltip = d3.select(chartRef.current)
+                        .append("div")
+                        .attr("class", "tooltip")
+                        .style("opacity", 0);
+
       // Map distances to an array
-      const distances = data.map(d => d.distance_raw);
+      const distances: number[] = data.map(d => d.distance_raw);
 
       // Define scales
       const xScale = d3.scaleBand()
-                       .domain(d3.range(distances.length))
+                       .domain(Array.from(Array(distances.length).keys()).map(String))
                        .range([0, 500])
                        .padding(0.1);
       
       const yScale = d3.scaleLinear()
-                       .domain([0, d3.max(distances)])
+                       .domain([0, d3.max(distances) as number])
                        .range([300, 0]);
 
       // Draw bars
-      svg.selectAll('rect')
+      const bars = svg.selectAll('rect')
          .data(distances)
          .enter()
          .append('rect')
-         .attr('x', (d, i) => xScale(i))
-         .attr('y', d => yScale(d))
+         .attr('x', (d, i) => xScale(String(i)) as number)
+         .attr('y', d => yScale(d) as number)
          .attr('width', xScale.bandwidth())
-         .attr('height', d => 300 - yScale(d))
+         .attr('height', d => 300 - yScale(d) as number)
          .attr('fill', 'blue');
+
+      // Add tooltips
+      bars.on("mouseover", function (event, d) {  // Notice the 'event' argument
+        tooltip.style("opacity", 1)
+              .html(`Distance: ${d}`)
+              .style("left", `${event.pageX}px`)
+              .style("top", `${event.pageY}px`);
+      })
+      .on("mouseout", function () {
+        tooltip.style("opacity", 0);
+      });
+
+
+      // Add click event to bars
+      bars.on("click", function (d: number, i: number) {
+          alert(`You clicked on bar ${i + 1} with distance ${d}`);
+      });
 
       // Add X-axis label
       svg.append('text')
